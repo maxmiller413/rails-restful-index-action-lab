ffaeb17381ef6d29cf740adbe4a3fc3f671cb7a9
Max Miller <maxmiller413@gmail.com>
2021-02-23 22:17:37 -0600

    commit tree e77c50785fd70e268cb343a4dc2336004ce96cdd

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,18 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  
+  get 'students/', to: 'students#index'
+  
+  
+  
+  
+  
+  
+  #resources :students, only: :index 
+  #get 'students', to: 'students#index'
+
+    #get 'posts/:id', to: 'posts#show'
+    # resources :posts, only: :show
+
 end
